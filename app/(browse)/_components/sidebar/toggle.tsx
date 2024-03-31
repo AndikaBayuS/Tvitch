b1a515f8e3fe6d5a3ff1ce98cdc7905f1765e0c9
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2024-03-31 15:40:25 +0700

    add Sidebar

---
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/store/use-sidebar";
+import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
+import { Hint } from "@/components/hint";
+
+export const Toggle = () => {
+  const { collapsed, onExpand, onCollapse } = useSidebar((state) => state);
+
+  const label = collapsed ? "Expand" : "Collapse";
+
+  return (
+    <>
+      {collapsed && (
+        <div className="items-center justify-center hidden w-full pt-4 mb-4 lg:flex">
+          <Hint label={label} side="right" asChild>
+            <Button onClick={onExpand} variant="ghost" className="h-auto p-2">
+              <ArrowRightFromLine className="w-4 h-4" />
+            </Button>
+          </Hint>
+        </div>
+      )}
+      {!collapsed && (
+        <div className="flex items-center w-full p-3 pl-6 mb-2">
+          <p className="font-semibold text-primary">For You</p>
+          <Hint label={label} side="right" asChild>
+            <Button
+              onClick={onCollapse}
+              className="h-auto p-2 ml-auto"
+              variant="ghost"
+            >
+              <ArrowLeftFromLine className="w-4 h-4" />
+            </Button>
+          </Hint>
+        </div>
+      )}
+    </>
+  );
+};
